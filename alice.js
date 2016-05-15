8703de3b1fa0393872af928c0b9da20bb723b9cb
uvalued <uvaluedbizinter@gmail.com>
2016-05-15 20:10:56 +0100

    Delete alice.js

--- alice.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('a').on('click',function(){
-        var aID = $(this).attr('href');
-        var elem = $(''+aID).html();
-        
-        $('.target').html(elem);
-    });
-});
